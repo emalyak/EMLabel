d7df4d3a2dab9516a06f1bf2d78310eb6fb43648
Erik Malyak <erik@skyhawkmedia.com>
2014-07-13 23:39:43 -0400

    Updated font size fitting algorithm, memory management

--- EMLabel/EMLabel.m
+++ EMLabel/EMLabel.m
@@ -30,34 +30,54 @@
 
 static CFAttributedStringRef CFAttributedStringCreateCopyWithAdjustedFontSize(CFAttributedStringRef attributedString, CGSize constrainingSize)
 {
-    CFMutableAttributedStringRef fittedAttributedString = CFAttributedStringCreateMutableCopy(kCFAllocatorDefault, 0, attributedString);
-    CTFontRef font = CFAttributedStringGetAttribute(fittedAttributedString, 0, kCTFontAttributeName, NULL);
+    CFMutableAttributedStringRef fittedAttributedString = CFAttributedStringCreateMutableCopy(NULL, 0, attributedString);
     
+    CTFontRef font = CFAttributedStringGetAttribute(fittedAttributedString, 0, kCTFontAttributeName, NULL);
     CGFloat fontSize = CTFontGetSize(font);
-    CGFloat calculatedFontSize = fontSize;
+    CFRelease(font);
     
     CTFramesetterRef framesetter = CTFramesetterCreateWithAttributedString(fittedAttributedString);
-    CGSize suggestedSize = CTFramesetterSuggestFrameSizeWithConstraints(framesetter, CFRangeMake(0, CFAttributedStringGetLength(fittedAttributedString)), NULL, CGSizeZero, NULL);
-    
-    if (suggestedSize.width > constrainingSize.width && fontSize > 0.0)
-    {
-        // Find change in suggested width per 1 point font size increase
-        font = CTFontCreateCopyWithAttributes(font, fontSize + 1.0, NULL, NULL);
-        CFAttributedStringSetAttribute(fittedAttributedString, CFRangeMake(0, CFAttributedStringGetLength(fittedAttributedString)), kCTFontAttributeName, font);
-        framesetter = CTFramesetterCreateWithAttributedString(fittedAttributedString);
-        CGSize upperSuggestedSize = CTFramesetterSuggestFrameSizeWithConstraints(framesetter, CFRangeMake(0, CFAttributedStringGetLength(fittedAttributedString)), NULL, CGSizeZero, NULL);
-        
-        float delta = upperSuggestedSize.width - suggestedSize.width;
-        
-        // Now calculate the new font size with the delta
-        calculatedFontSize = constrainingSize.width / delta;
-        
-        font = CTFontCreateCopyWithAttributes(font, calculatedFontSize, NULL, NULL);
-        CFAttributedStringSetAttribute(fittedAttributedString, CFRangeMake(0, CFAttributedStringGetLength(fittedAttributedString)), kCTFontAttributeName, font);
-    }
-    
-    CFRelease(font);
+    CGSize suggestedFrameSize = CTFramesetterSuggestFrameSizeWithConstraints(framesetter, CFRangeMake(0, CFAttributedStringGetLength(fittedAttributedString)), NULL, CGSizeZero, NULL);
     CFRelease(framesetter);
+    
+    CGSize calculatedFrameSize = suggestedFrameSize;
+    
+    if (suggestedFrameSize.width > constrainingSize.width && fontSize > 0.0)
+    {
+    
+        // Use binary search to find optimal scale factor
+        
+        float maxScaleFactor = 1.0f;
+        float minScaleFactor = 0;
+        
+        float accuracy = 0.01f;
+        
+        while ((maxScaleFactor - minScaleFactor) > accuracy)
+        {
+            float scaleFactorTest = (maxScaleFactor + minScaleFactor) / 2;
+            
+            font = CTFontCreateCopyWithAttributes(font, fontSize * scaleFactorTest, NULL, NULL);
+            CFAttributedStringSetAttribute(fittedAttributedString, CFRangeMake(0, CFAttributedStringGetLength(fittedAttributedString)), kCTFontAttributeName, font);
+            CFRelease(font);
+            
+            framesetter = CTFramesetterCreateWithAttributedString(fittedAttributedString);
+            calculatedFrameSize = CTFramesetterSuggestFrameSizeWithConstraints(framesetter, CFRangeMake(0, CFAttributedStringGetLength(fittedAttributedString)), NULL, CGSizeZero, NULL);
+            CFRelease(framesetter);
+            
+            if (calculatedFrameSize.width > constrainingSize.width)
+            {
+                maxScaleFactor = scaleFactorTest;
+            }
+            else
+            {
+                minScaleFactor = scaleFactorTest;
+            }
+        }
+        
+        font = CTFontCreateCopyWithAttributes(font, fontSize * minScaleFactor, NULL, NULL);
+        CFAttributedStringSetAttribute(fittedAttributedString, CFRangeMake(0, CFAttributedStringGetLength(fittedAttributedString)), kCTFontAttributeName, font);
+        CFRelease(font);
+    }
     
     return fittedAttributedString;
 }
@@ -85,7 +105,7 @@ static CGRect RectWithVerticalAlignment(CGSize frameSize, CGRect enclosingFrame,
 
 static CTFrameRef CFAttributedStringCreateFrame(CFAttributedStringRef attributedString, EMLabelVerticalAlignment verticalAlignment, CGRect frame, bool adjustSize)
 {
-    CFAttributedStringRef fittedAttributedString = CFAttributedStringCreateCopy(kCFAllocatorDefault, attributedString);
+    CFAttributedStringRef fittedAttributedString = CFAttributedStringCreateCopy(NULL, attributedString);
     
     if (adjustSize)
     {
@@ -166,23 +186,23 @@ static inline CTTextAlignment CTTextAlignmentFromNSTextAlignment(NSTextAlignment
     // Turn label values into attributed string
     CFStringRef keys[] = { kCTFontAttributeName, kCTForegroundColorAttributeName, kCTParagraphStyleAttributeName };
     CFTypeRef values[] = { (__bridge CTFontRef)(self.font), CGColorCreateCopy([self.textColor CGColor]), paragraphStyle };
+    CFRelease(paragraphStyle);
     
     CFDictionaryRef attributes =
-    CFDictionaryCreate(kCFAllocatorDefault, (const void**)&keys,
+    CFDictionaryCreate(NULL, (const void**)&keys,
                        (const void**)&values, sizeof(keys) / sizeof(keys[0]),
                        &kCFTypeDictionaryKeyCallBacks,
                        &kCFTypeDictionaryValueCallBacks);
     
-    CFAttributedStringRef attributedString = CFAttributedStringCreate(kCFAllocatorDefault, (__bridge CFStringRef)self.text, attributes);
-
+    CFAttributedStringRef attributedString = CFAttributedStringCreate(NULL, (__bridge CFStringRef)self.text, attributes);
     CFRelease(attributes);
     
     // Create and draw frame
     CTFrameRef textFrame = CFAttributedStringCreateFrame(attributedString, self.verticalAlignment, self.frame, self.adjustsFontSizeToFitWidth);
     CFRelease(attributedString);
+    
     CTFrameDraw(textFrame, context);
     CFRelease(textFrame);
-    CFRelease(paragraphStyle);
     
     CGContextRestoreGState(context);
 }
